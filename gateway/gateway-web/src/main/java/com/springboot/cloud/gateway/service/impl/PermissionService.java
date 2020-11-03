f17ffe9097c38682bdcc0483a48762b955c756a7
lijiangc <lijiangcheng@git.com>
2020-11-03 16:43:42 +0800

    格式调整

--- gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/service/impl/PermissionService.java
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/service/impl/PermissionService.java
@@ -1,10 +1,7 @@
 package com.springboot.cloud.gateway.service.impl;
 
-import com.alicp.jetcache.anno.CacheType;
-import com.alicp.jetcache.anno.Cached;
 import com.springboot.cloud.auth.client.service.IAuthService;
 import com.springboot.cloud.gateway.service.spec.IPermissionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +10,15 @@ public class PermissionService implements IPermissionService {
     /**
      * 由authentication-client模块提供签权的feign客户端
      */
-    @Autowired
-    private IAuthService authService;
+    private final IAuthService authService;
+
+    public PermissionService(IAuthService authService) {
+        this.authService = authService;
+    }
 
     @Override
-    @Cached(name = "gateway_auth::", key = "#authentication+#method+#url",
-            cacheType = CacheType.LOCAL, expire = 10, localLimit = 10000)
+    /*@Cached(name = "gateway_auth::", key = "#authentication+#method+#url",
+            cacheType = CacheType.LOCAL, expire = 10, localLimit = 10000)*/
     public boolean permission(String authentication, String url, String method) {
         return authService.hasPermission(authentication, url, method);
     }
