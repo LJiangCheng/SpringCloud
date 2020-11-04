9b2d255c542e42d5317538d2827cd2066c655fc7
lijiangc <lijiangcheng@git.com>
2020-11-04 15:41:53 +0800

    格式调整

--- gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/config/RequestRateLimiterConfig.java
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/config/RequestRateLimiterConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 /**
  * 自定义限流标志的key，多个维度可以从这里入手
  * exchange对象中获取服务ID、请求信息，用户信息等
@@ -41,6 +43,6 @@ public class RequestRateLimiterConfig {
      */
     @Bean
     public KeyResolver userKeyResolver() {
-        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("username"));
+        return exchange -> Mono.just(Objects.requireNonNull(exchange.getRequest().getQueryParams().getFirst("username")));
     }
 }
