b9753e550c7073f4b7cf0589b1ed8a50ae38bd37
lijiangc <lijiangcheng@git.com>
2020-11-06 14:55:01 +0800

    gateway-web添加TimerFilter

--- gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/filter/AccessGatewayFilter.java
+++ gateway/gateway-web/src/main/java/com/springboot/cloud/gateway/filter/AccessGatewayFilter.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -24,7 +25,7 @@ import reactor.core.publisher.Mono;
 @Configuration
 @ComponentScan(basePackages = "com.springboot.cloud.auth.client")
 @Slf4j
-public class AccessGatewayFilter implements GlobalFilter {
+public class AccessGatewayFilter implements GlobalFilter, Ordered {
 
     private static final String X_CLIENT_TOKEN_USER = "x-client-token-user";
     private static final String X_CLIENT_TOKEN = "x-client-token";
@@ -55,12 +56,11 @@ public class AccessGatewayFilter implements GlobalFilter {
         String authentication = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
         String method = request.getMethodValue();
         String url = request.getPath().value();
-        log.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
+        //log.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
         //不需要网关签权的url
         if (authService.ignoreAuthentication(url)) {
             return chain.filter(exchange);
         }
-
         //调用签权服务看用户是否有权限，若有权限进入下一个filter
         if (permissionService.permission(authentication, url, method)) {
             ServerHttpRequest.Builder builder = request.mutate();
@@ -100,5 +100,10 @@ public class AccessGatewayFilter implements GlobalFilter {
         DataBuffer buffer = serverWebExchange.getResponse()
                 .bufferFactory().wrap(HttpStatus.UNAUTHORIZED.getReasonPhrase().getBytes());
         return serverWebExchange.getResponse().writeWith(Flux.just(buffer));
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
     }
 }
